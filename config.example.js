4f550fb71dba4448d5d8ae7d2cc96c1f0c165a76
rusdi <rusdi.karsandi@gmail.com>
2018-11-24 01:08:07 +0700

    remove config and config example

--- config.example.js
+++ config.example.js
@@ -13,19 +13,4 @@
 // 	}
 // }
 
-// online
-var config = {
-	database: {
-		host:	  '128.199.223.13', 	// database host
-		user: 	  'rusdi.grevia', 		// your database username
-		password: 'rusdi!@#$2012', 		// your database password
-		port: 	  3306, 		// default MySQL port
-		db: 	  'test' 		// your database name
-	},
-	server: {
-		host: '127.0.0.1',
-		port: '3000'
-	}
-}
-
 module.exports = config
